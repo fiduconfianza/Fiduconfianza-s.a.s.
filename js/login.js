b58754b5c50aa1dd46cbcc594f02455f2fa3a34d
afgonzalez1 <146589844+afgonzalez1@users.noreply.github.com>
2024-07-01 17:41:22 -0500

    Add files via upload

---
+++ js/login.js
@@ -0,0 +1,41 @@
+function validarPassword() {
+    var usuario = document.login.usuario.value;
+    var password = document.login.password.value;
+
+    if (usuario === "" || password === "") {
+        alert('Todos los campos son obligatorios');
+        return false;
+    }
+
+    const validUsers = {
+        'didier23': 'saldo1.html',
+        'andres35': 'saldo2.html',
+        'carlos42': 'saldo3.html',
+        'javier23': 'saldo4.html',
+        'luisjk': 'saldo5.html',
+        'juan': 'saldo6.html',
+        'andre36': 'saldo7.html',
+        'carlos50': 'saldo8.html',
+        'javier6': 'saldo9.html',
+        'luisdf': 'saldo10.html'
+    };
+
+    if (validUsers.hasOwnProperty(usuario)) {
+        if (password === '92564368') {
+            alert('Bienvenido: ' + usuario);
+            window.location.href = validUsers[usuario];
+            return false; // Evitar el envío del formulario
+        } else {
+            alert('Usuario o clave incorrecta');
+            return false;
+        }
+    } else {
+        alert('Usuario o clave incorrecta');
+        return false;
+    }
+}
+
+    
+   
+        
+    
