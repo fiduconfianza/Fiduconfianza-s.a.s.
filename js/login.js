0924db6f5e02d47fb88c548f9bb42704544d1ed6
fiduconfianza <146589844+fiduconfianza@users.noreply.github.com>
2025-06-28 12:43:25 -0500

    Update login.js

--- js/login.js
+++ js/login.js
@@ -22,7 +22,7 @@ function validarPassword() {
         'glendyalvarado': 'saldo10.html',
         'albertc9': 'saldo11.html',
         'doragiron': 'saldo12.html',
-        'alvarofl': 'saldo13.html',
+        'pascual77': 'saldo13.html',
         'javier2': 'saldo14.html',
         'luisj': 'saldo15.html',
         'juan36': 'saldo16.html',
